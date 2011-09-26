9db3ba7e68aee03e7684b40e0be8b7230ea0c3a2
Richard Drake <rdrake@rdrake.org>
2011-09-26 05:13:17 -0400

    Added project description.

---
+++ src/project.clj
@@ -0,0 +1,6 @@
+(defproject Molly "0.0.1"
+            :description "Intelligent knowledge discovery system."
+            :url "http://rdrake.github.com/Molly"
+            :tasks []
+            :dependencies [[clojure "1.3.0"]
+                           [clucy "0.2.2"]])
