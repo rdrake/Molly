220c9dc51c8523a400a3b183838691001918ca6d
Richard Drake <rdrake@rdrake.org>
2012-07-19 19:37:29 -0400

    Benchmarking stuff.

--- perf.py
+++ perf.py
@@ -1,9 +1,11 @@
+import re
+
 from collections import defaultdict
 from matplotlib.pyplot import *
+from pbs import java
 
-import requests
-
-RUNS = 100
+# Some constants/settings
+RUNS = 2
 
 # Ken Pu
 FROM = "instructors|74"
@@ -12,31 +14,37 @@ FROM = "instructors|74"
 to = ["courses|csci_3030u", "instructors|75", "courses|educ_3482u"]
 
 # Plain, Atom, STM
-methods = ["plain", "atom", "stm"]
+#methods = ["plain", "atom", "stm"]
+methods = ["bfs-bench", "bfs-ref-bench", "bfs-atom-bench"]
 
 results = defaultdict(list)
 x = [i + 1 for i in range(RUNS)]
 
+# Should match the nn.nnn msec output of each command
+patt = re.compile("(\d+\.\d+)")
+
+# "Bake" some fixed parameters into the Java command
+java = java.bake("-server", "molly.core", c="mycampus.properties")
+
 for target in to:
-	clf() # Clear any existing plots
+	clf()
 	title("%s to %s" % (FROM, target))
 	xlabel("Run")
 	ylabel("Time (ms)")
 
 	for method in methods:
-
-		url = "http://localhost:8080/span?e0=%s&eL=%s&method=%s" % (FROM, target, methods)
-
-		print "\n%s -> %s" % (target, method)
-
 		for i in range(RUNS):
-			data = requests.get(url).json
-			results[(target, method)].append(data["debug"]["time"] / float(1000 * 1000))
+			# Output of the command must be converted to a string
+			output = str(java("--source", FROM, "--target", target, a=method))
+			# Grab the time taken
+			val = patt.search(output).group(1)
+
+			results[(target, method)].append(val)
 
 		plot(x, results[(target, method)], label=method)
-
+	
 	legend(loc="upper right")
-	ylim([0, 50])
+	#ylim([0, 100])
 	savefig("%s.png" % target)
 
 print results
--- src/molly/core.clj
+++ src/molly/core.clj
@@ -1,7 +1,6 @@
 (ns molly.core
   (:gen-class)
   (:use clojure.pprint
-        criterium.core
         molly.conf.config
         molly.server.serve
         molly.index.build
@@ -20,6 +19,33 @@
        ["-s" "--source" "Source node"]
        ["-t" "--target" "Target node"]))
 
+(def counter (atom 0))
+
+(defn ns-to-ms
+  [nanosec]
+  (/ nanosec (* 1000.0 1000.0)))
+
+(defn warmup
+  [func searcher source accept seconds]
+  "Takes in a function, searcher, source, accept function, and the number
+  of sections to run the function for"
+  (let [start     (System/nanoTime)
+        time-in-s (* seconds 1000 1000 1000)]
+    (while (< (- (System/nanoTime) start) time-in-s)
+      (func searcher source accept)
+      (swap! counter inc)))
+
+  @counter)
+;    (println (str (ns-to-ms (- (System/nanoTime) start)) " ms"))))
+
+(defn benchmark
+  [func searcher source accept]
+  "Benchmarks a function with a given searcher, source, and accept function"
+  (let [start (System/nanoTime)]
+    (func searcher source accept)
+    (let [elapsed (- (System/nanoTime) start)]
+      (println (str "Elapsed time: " (ns-to-ms elapsed) " msecs")))))
+
 (defn -main
   [& args]
   (let [[opts arguments banner] (parse-args (flatten args))
@@ -36,20 +62,25 @@
                                   (let [[marked hops] args]
                                     (or (some (fn [x]
                                                 (= x target)) marked)
-                                        (>= hops 5))))
-        bench'                  (fn [func]
-                                  (pprint
-                                    (with-progress-reporting
-                                      (benchmark
-                                        (func searcher source accept)))))]
+                                        (>= hops 25))))
+        bench                   (fn [func]
+                                  (warmup
+                                    func
+                                    searcher
+                                    "courses|alsu_1101u"
+                                    accept
+                                    30)
+                                  (benchmark func searcher source accept))]
     (condp = action
       "serve"     (start! properties)
       "index"     (build (properties :database)
                          (properties :index))
-      "bfs-atom-time" (time (bfs-atom searcher source accept))
-      "bfs-ref-time"  (time (bfs-ref searcher source accept))
-      "bfs-time"      (time (bfs searcher source accept))
-      "bfs-atom"      (bench' bfs-atom)
-      "bfs-ref"       (bench' bfs-ref)
-      "bfs"           (bench' bfs)
-      (println "I'm afraid I can't do that, Ken."))))
+      "bfs-atom-bench" (bench bfs-atom)
+      "bfs-ref-bench"  (bench bfs-ref)
+      "bfs-bench"      (bench bfs)
+      ;"bfs-atom"      (bench' bfs-atom)
+      ;"bfs-ref"       (bench' bfs-ref)
+      ;"bfs"           (bench' bfs)
+      (println "I'm afraid I can't do that, Ken."))
+    
+    (shutdown-agents)))
