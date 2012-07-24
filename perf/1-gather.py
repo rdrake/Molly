9f906c55c5cbac18cab31f0b9a903cb507e1b9c3
Richard Drake <rdrake@rdrake.org>
2012-07-24 02:37:45 -0400

    Modified perf to use new parameters.

--- perf/1-gather.py
+++ perf/1-gather.py
@@ -11,17 +11,24 @@ from matplotlib.pyplot import *
 from pbs import java
 
 # Some constants/settings
-RUNS = 1
+RUNS = 100
 
 # Ken Pu
 FROM = "instructors|74"
 
 # Database Systems, Jeremy Bradbury, Outdoor Education
-to = ["courses|csci_3030u", "instructors|75", "courses|educ_3482u"]
+to = [
+	"schedules|schedules|1731sections|610teaches|338",
+	"courses|engr_3960u",
+	"schedules|schedules|19657sections|10081teaches|9551",
+	"instructors|375",
+	"schedules|schedules|11068sections|4263teaches|3619",
+	"courses|csci_4100u",
+	"schedules|schedules|4125sections|1636teaches|1139",
+	"instructors|75"
+]
 
-# Plain, Atom, STM
-#methods = ["plain", "atom", "stm"]
-methods = ["bfs-bench", "bfs-ref-bench", "bfs-atom-bench"]
+methods = ["bfs", "bfs-atom", "bfs-ref"]
 
 results = defaultdict(list)
 x = range(RUNS)
@@ -30,17 +37,29 @@ x = range(RUNS)
 patt = re.compile("(\d+\.\d+)")
 
 # "Bake" some fixed parameters into the Java command
-java = java.bake("-server", "molly.core", c="mycampus.properties")
+java = java.bake(
+	"-server",
+	"molly.core",
+	"--source", FROM,
+	"--benchmark",
+	c="mycampus.properties")
+
+count = 0
 
 for target in to:
 	for method in methods:
 		for i in range(RUNS):
 			# Output of the command must be converted to a string
-			output = str(java("--source", FROM, "--target", target, a=method))
+			output = str(java("--target", target, "--algorithm", method))
 			# Gra) the time taken
 			val = patt.search(output).group(1)
 
 			results[(target, method)].append(val)
+
+			count += 1
+
+			if count % 100 == 0:
+				print "%d out of %d" % (count, RUNS)
 
 out = file("%s-result" % datetime.now(), "wb")
 
