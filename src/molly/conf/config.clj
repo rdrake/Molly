45a2dd321b396fee5d651896a06b5c19c923e1b8
Richard Drake <rdrake@rdrake.org>
2013-08-27 14:43:06 -0400

    Added default argument to load-props, playing around with d3.js as the web interface.

--- src/molly/conf/config.clj
+++ src/molly/conf/config.clj
@@ -2,18 +2,20 @@
   (:require clojure.java.io))
 
 (defn load-props
-  [file-name]
-  (let [res (clojure.java.io/resource file-name)]
-    (if (nil? res)
-      (throw
-        (IllegalArgumentException. "Unable to load properties."))
-      (with-open [^java.io.Reader reader
-                  (clojure.java.io/reader res)]
-        (let [props (java.util.Properties.)]
-          (.load props reader)
-          (into {}
-                (for [[k v] props] [(keyword k)
-                                    (read-string v)])))))))
+  ([]
+   (load-props ".properties"))
+  ([file-name]
+   (let [res (clojure.java.io/resource file-name)]
+     (if (nil? res)
+       (throw
+         (IllegalArgumentException. "Unable to load properties."))
+       (with-open [^java.io.Reader reader
+                   (clojure.java.io/reader res)]
+         (let [props (java.util.Properties.)]
+           (.load props reader)
+           (into {}
+                 (for [[k v] props] [(keyword k)
+                                     (read-string v)]))))))))
 
 (defprotocol IConfig
   (connection [this])
